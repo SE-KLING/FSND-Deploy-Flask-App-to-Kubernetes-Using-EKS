951f5cb009d6d31ed60e3d050f225edcf285b236
Stefan Klingenberg <stefan@byteorbit.com>
2020-05-26 08:24:15 +0200

    Added assert False to test_main.py to see if CodePipeline fails Added external_ip.txt to denote EXTERNAL_IP to be used

--- test_main.py
+++ test_main.py
@@ -24,6 +24,7 @@ def client():
 
 def test_health(client):
     response = client.get('/')
+    assert False #Testing CodePipeline
     assert response.status_code == 200
     assert response.json == 'Healthy'
 
